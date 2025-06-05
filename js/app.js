4528d5baa7212aee9c50de9bc1407697f9bedea7
Martín Pravia <marto.pravia@gmail.com>
2025-06-05 15:13:02 -0300

    ejercicio finalizado

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const allButtons = document.querySelectorAll(".button");
+
+for (const button of allButtons) {
+  button.addEventListener("click", function () {
+    const letter = this.textContent;
+    playSound(letter);
+  });
+}
+
+document.addEventListener("keydown", function (e) {
+  const keyDown = e.key;
+  playSound(keyDown);
+});
+
+function playSound(letter) {
+  const exactLetters = ["w", "a", "s", "d", "j", "k", "l"];
+  if (exactLetters.includes(letter)) {
+    const music = new Audio(`../sounds/${letter}.mp3`);
+
+    music.play();
+  }
+}
